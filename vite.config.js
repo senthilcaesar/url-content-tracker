c00770e777a69864b89cf44834917e3d5cefa142
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2026-03-19 00:21:52 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base =
+  process.env.VITE_BASE_PATH ||
+  (process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/')
+
 export default defineConfig({
+  base,
   plugins: [react()],
 })
